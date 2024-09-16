1d5424646bdf7f02a0028032c9e19233428c4236
NayhtSirc <cristhyan100@gmail.com>
2024-09-16 20:04:17 -0300

    Update login-cadastro.js

--- frontend/js/login-cadastro.js
+++ frontend/js/login-cadastro.js
@@ -12,3 +12,27 @@ cadastroBtn.addEventListener('click', () => {
     slideWindow.style.transform = 'translateX(-50%)';
     toggleSlider.style.transform = 'translateX(100%)';
 });
+
+
+//JS menu de recuperação de senha
+console.log('Arquivo login-cadastro.js carregado!');
+let logoMenu = document.getElementsByClassName('PwRecover')[0];
+let Esconder = document.querySelector('.menu');
+let voltar = document.querySelector('.back-icon');
+Esconder.style.display = 'none';
+logoMenu.addEventListener('click', function() {
+    console.log('A imagem do menu foi clicada!');
+    if (Esconder.style.display === 'none') {
+        Esconder.style.display = 'flex';
+    } else {
+        Esconder.style.display = 'none';
+    }
+});
+voltar.addEventListener('click', function() {
+    console.log('A imagem do menu1 foi clicada!');
+    if (Esconder.style.display === 'none') {
+        Esconder.style.display = 'flex';
+    } else {
+        Esconder.style.display = 'none';
+    }
+});
