85e4bd2288aba6d669774a595d51fbd0e5071ab8
NayhtSirc <cristhyan100@gmail.com>
2024-09-19 11:35:44 -0300

    Adicionado função de fechar menu de opções

--- frontend/js/pdv-principal.js
+++ frontend/js/pdv-principal.js
@@ -2,10 +2,19 @@ console.log('Arquivo pdv-principal.js carregado!');
 let logoSaida = document.querySelector('#perfil .saida');
 let logoMenu = document.getElementsByClassName('b3 scale')[0];
 let dropdownMenubackground = document.querySelector('.nav');
+let fecharmenu = document.querySelector('.back-icon');
 let Esconder = document.querySelector('.menu');
 Esconder.style.display = 'none';
 logoMenu.addEventListener('click', function() {
     console.log('A imagem do menu foi clicada!');
+    if (Esconder.style.display === 'none') {
+        Esconder.style.display = 'flex';
+    } else {
+        Esconder.style.display = 'none';
+    }
+});
+fecharmenu.addEventListener('click', function() {
+    console.log('A imagem do menu1 foi clicada!');
     if (Esconder.style.display === 'none') {
         Esconder.style.display = 'flex';
     } else {
